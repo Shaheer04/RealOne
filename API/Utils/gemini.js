592879f35a2ba0f377591fb755aa65ac31c204b2
SyedShahriyarAli <shahriyarali08@gmail.com>
2024-05-05 07:02:54 +0500

    Added Error Handling

--- API/Utils/gemini.js
+++ API/Utils/gemini.js
@@ -10,12 +10,17 @@ const model = genai.getGenerativeModel({ model: "gemini-pro" })
 const prompt = `Given product review data including customer reviews and images, along with product images provided by the seller,
      provide a summary and insights on whether new customers should purchase the product. Additionally, score the match between the
      delivered product and the shown product, comparing description images with review images on a scale of 1 to 10. 
-     Provide the response in JSON format with the following props {PositivePoints, NegativePoints, Insights, MatchScore}. Keep it short and concise.`;
+     Provide the response in JSON format with the following props {PositivePoints, NegativePoints, Insights, MatchScore}. 
+     Keep it short and concise. No need to add any sort of comments.`;
 
 async function analyseProductReviews(url) {
     var scrappedData = await scrapper.scrap(url);
-
-    const result = await model.generateContent([prompt, JSON.stringify(scrappedData)]);
+    try {
+        const result = await model.generateContent([prompt, JSON.stringify(scrappedData)]);
+        return JSON.parse(result.replaceAll('```', '').replace('JSON', '').replace('json', ''))
+    } catch (error) {
+        return { error: error.message }
+    }
     const response = await result.response;
 
     return JSON.parse(response.text().replaceAll('```', '').replace('JSON', '').replace('json', ''))
--- Extension/script.js
+++ Extension/script.js
@@ -1,29 +1,48 @@
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//     if (tab.url.match(/daraz\.pk./)) {
+//         document.getElementsByClassName('.text')[0].style.display = 'none';
+//     } else {
+//         document.getElementsByClassName('.complete-analysis')[0].style.display = 'none';
+//     }
+// })
+
 const myButton = document.getElementById('myButton');
 
 myButton.addEventListener('click', async function () {
+
     const [currentTab] = await chrome.tabs.query({
         active: true,
         currentWindow: true,
     });
 
-    sendUrlToGeminiApi(currentTab.url);
+    myButton.style.cssText = 'display:none;'
+    document.querySelector('.spinner').style.cssText = 'display:block;'
+    await sendUrlToGeminiApi(encodeURIComponent(currentTab.url));
 
 });
 
-function sendUrlToGeminiApi(url) {
+async function sendUrlToGeminiApi(url) {
     const apiUrl = `http://localhost:3000?url=${url}`;
 
     fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+        .then((response) => response.text())
+        .then((result) => {
+
+            var response = JSON.parse(result.toString());
+            if (response.error) {
+                console.error(response.error);
+                alert('Error: ' + response.error)
+                return;
             }
-            return response.json();
+
+            document.getElementById('positivePoints').innerText = response.PositivePoints;
+            document.getElementById('negativePoints').innerText = response.NegativePoints;
+            document.getElementById('insights').innerText = response.Insights;
+            document.getElementById('score').innerText = response.MatchScore;
+
+            document.getElementsByClassName('gauge')[0].style.cssText = `width: 300px; --rotation:${response.MatchScore * 18}deg; --color:#5cb85c; --background:#e9ecef;`;
+            myButton.style.cssText = 'display:block;'
+            document.querySelector('.spinner').style.cssText = 'display:none;'
         })
-        .then(data => {
-            console.log('Response from Gemini API:', data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+        .catch((error) => console.error(error));
 }
